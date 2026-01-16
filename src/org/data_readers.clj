5a9e3d71eb25f2a95ee3e4ba16e479e70687c446
Christian Weilbach <christian@weilbach.name>
2026-01-15 21:47:02 -0800

    Update data_readers.clj to use org.replikativ namespace

--- src/org/data_readers.clj
+++ src/org/data_readers.clj
@@ -1,2 +1,2 @@
-{geheimnis/Base64 geheimnis.base64/str->base64
- geheimnis/RSAKey geheimnis.rsa/base64map->RSAKey}
+{geheimnis/Base64 org.replikativ.geheimnis.base64/str->base64
+ geheimnis/RSAKey org.replikativ.geheimnis.rsa/base64map->RSAKey}
